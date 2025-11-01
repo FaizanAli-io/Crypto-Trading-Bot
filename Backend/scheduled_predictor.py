23a99345fbbc483333444ab9b6fb9fbb95253229
Bilal <imbilal2004@gmail.com>
2025-11-01 20:42:04 +0500

    xg_predict Predictor3

--- Backend/scheduled_predictor.py
+++ Backend/scheduled_predictor.py
@@ -24,7 +24,7 @@ WHATSAPP_NUMBERS = [
     # "+9876543210",
 ]
 
-HIGH_CONFIDENCE_THRESHOLD = 0.5  # 75% confidence for WhatsApp alerts
+HIGH_CONFIDENCE_THRESHOLD = 0.75  # 75% confidence for WhatsApp alerts
 
 # Symbols to monitor
 SYMBOLS = ["BTCUSDT", "ETHUSDT", "BNBUSDT", "XRPUSDT", "ADAUSDT", 
